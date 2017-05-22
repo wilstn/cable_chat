1473be414e2f290606ba81cf35b1556bf8d19231
icebear <wilsontan.2088@gmail.com>
2017-05-22 16:40:28 +0800

    Part of previous commit

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -19,6 +19,7 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
 
   private
