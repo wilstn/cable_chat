4429f5d89d600a16e09fdb10867c539f3bfa4e08
icebear <wilsontan.2088@gmail.com>
2017-05-22 17:45:41 +0800

    Create a job to broadcast message after saving of send_message's message

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,8 @@ class Message < ApplicationRecord
 
   validates :body, presence: true, length: { minimum: 2, maximum: 1000 }
 
+  after_create_commit { MessageBroadcastJob.perform_later(self) }
+
   def timestamp
     created_at.strftime('%H:%M:%S %d %B %Y')
   end
