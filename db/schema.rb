9e9074ede72b0b7ae0d973df010c1234d0ae8c5d
icebear <wilsontan.2088@gmail.com>
2017-05-22 15:31:53 +0800

    Create chatroom model and add relevant associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522072731) do
+ActiveRecord::Schema.define(version: 20170522072956) do
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_chat_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
